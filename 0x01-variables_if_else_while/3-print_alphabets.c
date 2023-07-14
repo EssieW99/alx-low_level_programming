1e7696bf81131a074d208180d935df2252234d7e
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-07-14 10:45:32 -0700

    Alphabetic

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch, ab;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+	for (ab = 'A'; ab <= 'Z'; ab++)
+
+	putchar(ch);
+	putchar(ab);
+	putchar('\n');
+
+	return (0);
+}
