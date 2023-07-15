8b4e166314608f4219142616a5e5610f6936fd6c
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-07-14 18:48:57 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,16 @@ int main(void)
 	int a;
 
 	for (a = 0; a <= 9; a++)
-
+	{
 	putchar(a + '0');
 
-	if (a < 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		if (a < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	putchar('\n');
+	}
 
 	return (0);
 }
