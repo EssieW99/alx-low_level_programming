776e3a20a38860c17242658e4c81d8444cea765f
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-07-19 08:53:58 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Starting point
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int b, c;
+
+	for (b = 0; b <= 10; b++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
