4ada52aaf45fb56faec494f7ba869678f0a82c7f
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-07-20 07:49:03 -0700

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_isupper - Checks for an uppercase letter
+ *@c: The character being checked
+ *
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	_isupper(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
