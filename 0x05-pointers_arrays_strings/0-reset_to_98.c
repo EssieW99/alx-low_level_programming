8b0b97abfc7339a1136a3ae6618618aaf40c9782
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-07-25 03:09:37 -0700

    Reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - Takes a pointer and updates the value it points
- *@n: The number being updated
+ *@n: The value of a dereferencing pointer
  *
  */
 void reset_to_98(int *n)
 {
-	int a = 402;
-	int *ptr = &a;
-
-	*ptr = 98;
-
+	*n = 98;
 }
