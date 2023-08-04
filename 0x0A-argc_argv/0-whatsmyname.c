8e18a2d1fb46941222e8faded55ccdc7312b0d10
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-04 09:55:33 -0700

    Prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints own name
+ *@argc: Counts number of command line arguements
+ *@argv[]: Array containing command arguments
+ *
+ * Return: Is 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
