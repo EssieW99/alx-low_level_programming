0d92997ed6d3ef0a11e7fb53223aa47b3396256b
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-04 10:17:38 -0700

    Print number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@argc: Counts number of command line arguments
+ *@argv[]: An array containing command line arguments
+ *@argv: Contains command line arguments
+ *
+ * Return: Is 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
