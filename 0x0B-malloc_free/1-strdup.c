1c761680fbac9693284efb7d69b4948bc535f147
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-07 10:06:41 -0700

    Copying a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Returns a pointer to a new string
+ * @str: String input
+ *
+ * Return: Is NULL or ptr
+ */
+char *_strdup(char *str)
+{
+	unsigned int length = strlen(str);
+
+	unsigned int i;
+
+	char *ptr = (char *)malloc(sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= length; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+	free(ptr);
+}
