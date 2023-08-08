04adbb25bf1e238ece8488f48bc1fac58d839459
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-08 10:12:19 -0700

    Locate a grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - A pointer to a two dimensional array of integers
+ * @width: Width of the grid
+ * @height: Height of the grid
+ *
+ * Return: Is NULL or value of ptr
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **)malloc(height * sizeof(int *));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = (int *)malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ptr[j]);
+			}
+		return (NULL);
+		free(ptr);
+		}
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
