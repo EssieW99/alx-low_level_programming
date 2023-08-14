920e4018f76c15100f76ae87123a30f3e8b43cf0
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-14 09:54:53 -0700

    Define a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_HEADER_FILE
 #define DOG_HEADER_FILE
 
-struct dog {
+/**
+ * struct dog - Details about a dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dogs's owner
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+} my_dog;
 
 #endif
