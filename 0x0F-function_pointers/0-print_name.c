d2d2c50cc3bc775362edf8ca1d9927c4ee7b2b24
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-16 07:07:04 -0700

    Print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <string.h>
 
 /**
  * print_name - Prints a name
@@ -8,5 +9,9 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-		f(name);
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
