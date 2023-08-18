f699a815412c4960e440390a3b919c91f49587c8
Esther Wanjiku <esther.wanjikum99@gmail.com>
2023-08-18 09:11:36 -0700

    A calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns sum of a and b
+ * @a: One of the integers
+ * @b: The other number
+ *
+ * Return: Value of a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_add - Calculates difference of a and b
+ * @a: One of the numbers
+ * @b: The other number
+ *
+ * Return: Is the value of a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies a and b
+ * @a: One of the numbers
+ * @b: The other number
+ *
+ * Return: Is the value of a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides a by b
+ * @a: One of the numbers
+ * @b: The other number
+ *
+ * Return: The value of a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Calculates the remainder of the division of a by b
+ * @a: One of the numbers
+ * @b: The other number
+ *
+ * Return: Is the value of a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
